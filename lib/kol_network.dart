ef5a353ffde9e7bf9f6df63e13fbae6b3eebf652
ajoshi <ayj5012@gmail.com>
2018-08-08 23:55:10 -0700

    Network now fetches player data from api.php

--- lib/kol_network.dart
+++ lib/kol_network.dart
@@ -58,7 +58,7 @@ class KolNetwork {
       var realLoginRequest = await httpClient.postUrl(tempUri);
       var realResponse = await realLoginRequest.close();
 
-      updateCookies(realResponse.cookies);
+      _updateCookies(realResponse.cookies);
       httpClient.close(force: false);
 
       if (_phpsessid == null) {
@@ -66,7 +66,7 @@ class KolNetwork {
       }
 
       // get the charpwd as well so we can make arbitrary requests
-      await getPlayerData();
+      await _getCharPwdAndHash();
 
       _isLoggedIn = true;
       // tell the consumer we logged in
@@ -78,8 +78,8 @@ class KolNetwork {
   }
 
   /// Logging in doesn't get us all the player data, but hitting the charpane does
-  /// So we check the charpane for the pwdhash (and get the player id just in case)
-  Future<bool> getPlayerData() async {
+  /// So we check the charpane for the pwdhash and charpwd
+  Future<bool> _getCharPwdAndHash() async {
     var response = await makeRequest("charpane.php?$_forAppName");
     if (response.responseCode == NetworkResponseCode.SUCCESS) {
       var charInfoHtml = response.response;
@@ -91,6 +91,56 @@ class KolNetwork {
       return true;
     }
     return false;
+  }
+
+  /// Fetches player data from api.php. Returns null on failure (bad network?)
+  Future<Map> getPlayerData() async {
+    var statusresponse =
+        await makeRequestWithQueryParams("api.php", "what=status");
+    //{"playerid":"2129446","name":"ajoshi","hardcore":"1","ascensions":"319",
+    // "path":"22","sign":"Vole","roninleft":"308","casual":"0","drunk":"13",
+    // "full":"4","turnsplayed":"760080","familiar":"213","hp":"359","mp":"54",
+    // "meat":"95332","adventures":"42","level":"14","rawmuscle":"12920",
+
+    // "rawmysticality":"13323","rawmoxie":"32584","basemuscle":"113",
+    // "basemysticality":"115","basemoxie":"180","familiarexp":400,"class":"6",
+    // "lastadv":{"id":"1026","name":"The Naughty Sorceress' Tower",
+    // "link":"place.php?whichplace=nstower","container":"place.php?whichplace=nstower"},
+    // "title":"14","pvpfights":"70","maxhp":394,"maxmp":446,"spleen":"0",
+    // "muscle":223,"mysticality":225,"moxie":345,"famlevel":25,"locked":false,
+    // "limitmode":0,"daysthisrun":"4","equipment":{"hat":"2069","shirt":"6719",
+    // "pants":"9574","weapon":"6815","offhand":"9133","acc1":"5039","acc2":"7967",
+    // "acc3":"9322","container":"9082","familiarequip":"2573","fakehands":0,"cardsleeve":0},
+    // "stickers":[0,0,0],"soulsauce":0,"fury":0,"pastathrall":0,"pastathralllevel":1,
+    // "folder_holder":["17","15","22","00","00"],"eleronkey":"<SOME HASH>",
+    // "flag_config":{"noinvpops":0,"fastdecking":"1","devskills":0,"shortcharpane":0,
+    // "lazyinventory":0,"compactfights":"1","poppvpsearch":0,"questtracker":0,
+    // "charpanepvp":"1","australia":"1","fffights":"1","compactchar":0,"noframesize":0,
+    // "fullnesscounter":"1","nodevdebug":0,"noquestnudge":0,"nocalendar":0,"alwaystag":0,
+    // "clanlogins":"1","quickskills":"1","hprestorers":0,"hidejacko":0,"anchorshelf":0,
+    // "showoutfit":0,"wowbar":"1","swapfam":0,"hidefamfilter":0,"invimages":0,
+    // "showhandedness":0,"acclinks":"1","invadvancedsort":"1","powersort":0,
+    // "autodiscard":0,"unfamequip":"1","invclose":0,"sellstuffugly":0,
+    // "oneclickcraft":0,"dontscroll":0,"multisume":"1","threecolinv":"1","profanity":"1",
+    // "tc_updatetitle":0,"tc_alwayswho":0,"tc_times":0,"tc_combineallpublic":0,
+    // "tc_eventsactive":0,"tc_hidebadges":0,"tc_colortabs":0,"tc_modifierkey":0,
+    // "tc_tabsonbottom":0,"chatversion":"1","aabosses":0,"compacteffects":0,
+    // "slimhpmpdisplay":"1","ignorezonewarnings":"1","whichpenpal":"4",
+    // "compactmanuel":"1","hideefarrows":0,"questtrackertiny":0,"questtrackerscroll":0,
+    // "disablelovebugs":0,"eternalmrj":"1","autoattack":0,"topmenu":0},"recalledskills":0,
+    // "freedralph":0,"mcd":0,"pwd":"a629273e74c4cef59001974fa47a8556",
+    // "rollover":1533353398,"turnsthisrun":692,"familiar_wellfed":0,
+    // "intrinsics":{"518f53443c261c2b61ea11fe8716a715":["Spirit of Peppermint",
+    // "snowflake","518f53443c261c2b61ea11fe8716a715","168"]},"familiarpic":"xoskeleton",
+    // "pathname":"Standard",
+    // "effects":{"0bf172ccba65be4fdc4c0f908325b5c1":["Everything Looks Yellow",66,"eyes",null,790]}}
+    if (statusresponse.responseCode == NetworkResponseCode.SUCCESS) {
+      Map parsedResponse = json.decode(statusresponse.response);
+      //int maxMp = parsedResponse["maxmp"];
+      //     var advs = parsedResponse["adventures"];
+      return parsedResponse;
+    }
+    return null;
   }
 
   /// Given a bigString, finds the substring between the two passed in Strings
@@ -130,7 +180,7 @@ class KolNetwork {
   /// Make a network request for a given url. Defaults to GET, but can make PUT requests as well
   Future<NetworkResponse> makeRequest(String url,
       {HttpMethod method = HttpMethod.GET}) async {
-  //  print("call to $url");
+    //  print("call to $url");
     try {
       var httpClient = new HttpClient();
       var headerCookie =
@@ -161,7 +211,7 @@ class KolNetwork {
         }
       }
       // login.php?invalid=1
-      updateCookies(resp.cookies);
+      _updateCookies(resp.cookies);
 
       //  TODO handle network failures while making request
       try {
@@ -177,7 +227,7 @@ class KolNetwork {
   }
 
   /// Call this with the server cookie response so we can update our cookies
-  void updateCookies(List<Cookie> cookies) {
+  void _updateCookies(List<Cookie> cookies) {
     for (var cook in cookies) {
       if (cook.name == "PHPSESSID") {
         _phpsessid = cook.value;
