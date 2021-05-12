de15184521b99496ab625226d64db635baa49267
ajoshi <721945+ajoshi@users.noreply.github.com>
2021-05-11 22:20:44 -0700

    Updating to android embedding v2 (new way of flutter components being embedded)

--- android/app/src/main/kotlin/biz/ajoshi/kolminer/MainActivity.kt
+++ android/app/src/main/kotlin/biz/ajoshi/kolminer/MainActivity.kt
@@ -2,12 +2,12 @@ package biz.ajoshi.kolminer
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity(): FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity: FlutterActivity() {
+//  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+//    GeneratedPluginRegistrant.registerWith(flutterEngine);
+//  }
 }
