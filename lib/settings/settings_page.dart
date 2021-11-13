0bf87f5552660111e77fba8eb0190267f305e951
ajoshi <721945+ajoshi@users.noreply.github.com>
2021-11-12 22:32:37 -0800

    More accessibility

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -20,6 +20,7 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends DisposableHostState<SettingsPage> {
   Settings? _settings;
+
   // Stick all controllers in a map so we don't recreate them every time build happens
   final Map<String, SafeTextEditingController> _textEditingControllerMap =
       new HashMap();
@@ -42,16 +43,26 @@ class _SettingsPageState extends DisposableHostState<SettingsPage> {
   }
 
   Widget _actionIdInputRow(Setting? setting, String hintText) {
-    return _inputRow(setting, hintText,
-        TextInputType.numberWithOptions(signed: false, decimal: false), " id");
+    return _inputRow(
+        setting,
+        hintText,
+        TextInputType.numberWithOptions(signed: false, decimal: false),
+        " ID",
+        "ID");
   }
 
-  Widget _chatInputRow(Setting? setting, String hintText) {
-    return _inputRow(setting, hintText, TextInputType.text, " command");
+  Widget _chatInputRow(
+      Setting? setting, String hintText, String semanticsLabel) {
+    return _inputRow(
+        setting, hintText, TextInputType.text, " command", semanticsLabel);
   }
 
-  Widget _inputRow(Setting? setting, String hintText,
-      TextInputType secondInputType, String secondInputHintSuffix) {
+  Widget _inputRow(
+      Setting? setting,
+      String hintText,
+      TextInputType secondInputType,
+      String secondInputHintSuffix,
+      String semanticsLabel) {
     if (setting == null) {
       return Container();
     }
@@ -62,11 +73,14 @@ class _SettingsPageState extends DisposableHostState<SettingsPage> {
     return new Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          ConstrainedBox(
-            child: new Text(hintText,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.overline),
-            constraints: const BoxConstraints(minWidth: 40),
+          MergeSemantics(
+            child: ConstrainedBox(
+              child: new Text(hintText,
+                  semanticsLabel: semanticsLabel,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.overline),
+              constraints: const BoxConstraints(minWidth: 40),
+            ),
           ),
           new SettingTextInputField("Button name", TextInputType.text, (value) {
             setting.name = value;
@@ -107,27 +121,30 @@ class _SettingsPageState extends DisposableHostState<SettingsPage> {
       child: new Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: new Text(
-                "Save commonly used food/booze/skills as buttons for easier use",
-                //https://www.kingdomofloathing.com/inv_eat.php?pwd=f22d1ea8c998551f6ce74de08c89172e&which=1&whichitem=5483
-                style: Theme.of(context).textTheme.bodyText2),
+          MergeSemantics(
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: new Text(
+                  "Save commonly used food/booze/skills as buttons for easier use",
+                  style: Theme.of(context).textTheme.bodyText2),
+            ),
           ),
           _actionIdInputRow(_settings?.food, "Food"),
           _actionIdInputRow(_settings?.booze, "Booze"),
           _actionIdInputRow(_settings?.skill, "Skill"),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 4.0, top: 12.0, right: 4.0, bottom: 4.0),
-            child: new Text(
-                'Save up to three commonly used chat commands without the slash (eg. \"outfit roll\") for easier use',
-                style: Theme.of(context).textTheme.bodyText2),
+          MergeSemantics(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 4.0, top: 12.0, right: 4.0, bottom: 4.0),
+              child: new Text(
+                  'Save up to three commonly used chat commands without the slash. (eg. \"outfit roll\") for easier use',
+                  style: Theme.of(context).textTheme.bodyText2),
+            ),
           ),
-          _chatInputRow(_settings?.chat1, "Cmd 1"),
-          _chatInputRow(_settings?.chat2, "Cmd 2"),
-          _chatInputRow(_settings?.chat3, "Cmd 3"),
-          _chatInputRow(_settings?.chat4, "Cmd 4"),
+          _chatInputRow(_settings?.chat1, "Cmd 1", "Chat command 1"),
+          _chatInputRow(_settings?.chat2, "Cmd 2", "Chat command 2"),
+          _chatInputRow(_settings?.chat3, "Cmd 3", "Chat command 3"),
+          _chatInputRow(_settings?.chat4, "Cmd 4", "Chat command 4"),
           Center(
             child: Padding(
               padding: const EdgeInsets.all(10.0),
